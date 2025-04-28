99dc1602874e011f092ddc671c952ed44fa3da99
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 17:57:30 -0400

    chore: Install React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App"
+import { BrowserRouter } from "react-router";
+import App from "./components/App/App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
