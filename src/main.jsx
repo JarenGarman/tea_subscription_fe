5c2534c639ecfb36ebd4ee9dc2431f2f277fb7ee
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 17:58:57 -0400

    chore: Add back StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./components/App/App";
@@ -5,6 +6,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <StrictMode>
+      <App />
+    </StrictMode>
   </BrowserRouter>
 );
