6bf3c21223989a1142e3ea1718441f254b4bc1e5
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 18:07:41 -0400

    feat: Add Links and Routes to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,40 @@
+import { Link, NavLink, Route, Routes, useMatch } from "react-router";
+import Details from "../Details/Details";
+import Home from "../Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Tea Subscription</h1>
+      <main>
+        <header>
+          <nav>
+            <NavLink to="/" data-cy="home">
+              <h1 data-cy="title">Tea Subscriptions</h1>
+            </NavLink>
+            {useMatch("/:id") && (
+              <Link to="/" data-cy="home">
+                ⌂
+              </Link>
+            )}
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Details />} />
+          <Route
+            path="*"
+            element={
+              <section className="error-message" data-cy="error-message">
+                <h2>404: This page doesn't exist</h2>
+                <p>Sorry, the route you're trying to visit is invalid</p>
+                <Link to="/">Go back home</Link>
+              </section>
+            }
+          />
+        </Routes>
+      </main>
     </>
   );
 }
