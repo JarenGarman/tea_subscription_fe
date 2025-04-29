41fb0163fd5c8ac6d6f246941f76762ee5242248
Jaren Garman <jarengarman@protonmail.com>
2025-04-29 12:02:29 -0400

    feat: Add skeleton for Home and Subscription

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { getSubscriptions } from "../../apiCalls";
+import Subscription from "../Subscription/Subscription";
 import "./Home.css";
 
 function Home() {
-  return;
+  const [subscriptions, setSubscriptions] = useState([]);
+  let subComponents
+
+  useEffect(() => {
+    getSubscriptions().then((subscriptions) =>
+      setSubscriptions(subscriptions.data)
+    );
+  }, []);
+
+  subComponents = subscriptions.map((sub) => {
+    return (
+      <Subscription
+        key={sub.id}
+        id={sub.id}
+        title={sub.title}
+        price={sub.price}
+        image_url={sub.image_url}
+      />
+    );
+  });
+
+  if (subscriptions.length === 0) {
+    return <h2>Loading...</h2>;
+  }
+
+  return <div className="subscriptions-container">{subComponents}</div>;
 }
 
 export default Home;
