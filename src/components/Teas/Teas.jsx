290b69f903b04f1e01f307306683303175e1a60b
Jaren Garman <jarengarman@protonmail.com>
2025-04-29 12:59:56 -0400

    feat: Flesh out Teas component

--- src/components/Teas/Teas.jsx
+++ src/components/Teas/Teas.jsx
@@ -1,7 +1,24 @@
 import "./Teas.css";
+import Tea from "../Tea/Tea"
 
-function Teas() {
-  return;
+function Teas({ teas }) {
+  const teaComponents = teas.map((tea) => {
+    return <Tea
+      key={tea.id}
+      id={tea.id}
+      title={tea.attributes.title}
+      description={tea.attributes.description}
+      brew_time={tea.attributes.brew_time}
+      image_url={tea.attributes.image_url}
+    />
+  })
+
+  return (
+    <section className="teas-section">
+      <h2>Subscription Teas</h2>
+      {teaComponents}
+    </section>
+  );
 }
 
 export default Teas;
