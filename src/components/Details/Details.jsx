873b965a37ac3244e4b9375f0a88d065b8263029
Jaren Garman <jarengarman@protonmail.com>
2025-04-29 12:52:01 -0400

    feat: Add subscription details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getSubscription } from "../../apiCalls";
+import CustomerSubscriptions from "../CustomerSubscriptions/CustomerSubscriptions";
+import DetailedSubscription from "../DetailedSubscription/DetailedSubscription";
+import Teas from "../Teas/Teas";
 import "./Details.css";
 
 function Details() {
-  return;
+  const [sub, setSub] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    getSubscription(params.id).then((subscription) => setSub(subscription));
+  }, [params.id]);
+
+  if (!sub) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <article className="subscription-details">
+      <DetailedSubscription
+        title={sub.data.attributes.title}
+        price={sub.data.attributes.price}
+        image_url={sub.data.attributes.image_url}
+      />
+      <Teas />
+      <CustomerSubscriptions />
+    </article>
+  );
 }
 
 export default Details;
