6cbfc3e13419fbdf59d45ab1d9fa5af22bf72927
Jaren Garman <jarengarman@protonmail.com>
2025-04-30 12:21:20 -0400

    test: Add tests for default sorting order

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,13 +1,41 @@
 describe("Main Page", () => {
   beforeEach(() => {
-    cy.intercept("GET", "/api/v1/subscriptions", { fixture: "subs" }).as(
-      "getSubs"
-    );
-    cy.visit("http://localhost:5173/").wait("@getSubs");
+    cy.intercept("GET", "/api/v1/subscriptions", { fixture: "subs" })
+      .as("getSubs")
+      .visit("http://localhost:5173/")
+      .wait("@getSubs");
   });
+
   it("Displays header with title and sort", () => {
-    cy.getBySel("title").should("contain", "Tea Subscriptions");
-    cy.getBySel("sort-label").should("contain", "Sort by: ");
-    cy.getBySel("sort-select").should("exist");
+    cy.getBySel("title")
+      .should("contain", "Tea Subscriptions")
+
+      .getBySel("sort-label")
+      .should("contain", "Sort by: ")
+
+      .getBySel("sort-select")
+      .should("exist");
+  });
+
+  it("Displays subscriptions in default order", () => {
+    cy.getBySel("subscriptions-container")
+      .find(".sub-title")
+      .first()
+      .should("contain", "commodi")
+
+      .getBySel("subscriptions-container")
+      .find(".sub-price")
+      .first()
+      .should("contain", "$83.12")
+
+      .getBySel("subscriptions-container")
+      .find(".sub-title")
+      .last()
+      .should("contain", "neque")
+
+      .getBySel("subscriptions-container")
+      .find(".sub-price")
+      .last()
+      .should("contain", "$24.36");
   });
 });
